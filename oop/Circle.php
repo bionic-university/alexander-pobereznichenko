b63e928322fe15b9160feab3e377855589fd1344
sania <apobereznichenko@gmail.com>
2014-04-28 19:04:44 +0300

    create class Circle

--- oop/Circle.php
+++ oop/Circle.php
@@ -1,7 +1,14 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: sania
  * Date: 28.04.14
  * Time: 18:37
- */ 
+ */
+class Circle
+{
+    public $material;
+    public $radius;
+    public $color;
+}
