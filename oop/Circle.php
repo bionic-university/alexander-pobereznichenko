a1091e23ec5982674a83dae27de44a7eeb60f5b5
sania <apobereznichenko@gmail.com>
2014-04-28 19:34:45 +0300

    get and set methods for Circle class

--- oop/Circle.php
+++ oop/Circle.php
@@ -9,6 +9,53 @@
 class Circle
 {
     public $material;
+
+    /**
+     * @var float $radius
+     */
     public $radius;
+
+    /**
+     * @var string $color
+     */
     public $color;
+
+    /**
+     * @return string
+     */
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    /**
+     * @param string $color
+     */
+    public function setColor($color)
+    {
+        $this->color = $color;
+    }
+
+    /**
+     * @return float
+     */
+    public function getRadius()
+    {
+        return $this->radius;
+    }
+
+    /**
+     * @param float $radius
+     */
+    public function setRadius($radius)
+    {
+        $this->radius = $radius;
+    }
+
+    public function  __constructor($material, $radius, $color)
+    {
+        $this->$material = $material;
+        $this->$radius = $material;
+        $this->$color = $material;
+    }
 }
