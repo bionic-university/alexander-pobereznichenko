7c9bcea2018168cd286a901495d9d66b85105734
sania <apobereznichenko@gmail.com>
2014-04-29 21:17:55 +0300

    counter with static property $counter example

--- oop/homework/classes/TownHouse.php
+++ oop/homework/classes/TownHouse.php
@@ -1,12 +1,15 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: sania
  * Date: 29.04.14
  * Time: 8:47
  */
+class TownHouse extends AbstractBuilding
+{
+    static $counter = 0;
 
-class TownHouse extends AbstractBuilding{
     public function approximateSellPrice()
     {
         // TODO: Implement approximateSellPrice() method.
@@ -15,6 +18,28 @@ class TownHouse extends AbstractBuilding{
     public function approximateRentPrice()
     {
         // TODO: Implement approximateRentPrice() method.
+    }
+
+    /**
+     * @return int
+     */
+    public static function getCounter()
+    {
+        return self::$counter;
+    }
+
+    /**
+     * @param int $counter
+     */
+    public static function setCounter($counter)
+    {
+        self::$counter = $counter;
+    }
+
+
+    public function __construct()
+    {
+        self::$counter += 1;
     }
 
 } 
--- oop/homework/index.php
+++ oop/homework/index.php
@@ -4,4 +4,17 @@
  * User: sania
  * Date: 29.04.14
  * Time: 18:52
- */ 
+ */
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+include 'classes/AbstractBuilding.php';
+include 'classes/TownHouse.php';
+
+$a = new TownHouse();
+$b = new TownHouse();
+$c = new TownHouse();
+$d = new TownHouse();
+$e = new TownHouse();
+
+
+echo $b->getCounter();
